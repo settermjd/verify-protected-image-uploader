656c97788b72775a5f27aaa8199d5836aeafd9e0
Matthew Setter <matthew@matthewsetter.com>
2025-05-28 13:31:57 +1000

    Add a service class to simplify interacting with Twilio's Verify API
    This, as the the commit title says, is there to simplify interacting
    with Twilio's Verify API for sending and verifying validation codes.
    Also, it just makes testing the handlers that much simpler.

---
+++ src/App/Service/TwilioVerificationService.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Twilio\Rest\Client;
+use Twilio\Rest\Verify\V2\Service\VerificationCheckInstance;
+use Twilio\Rest\Verify\V2\Service\VerificationInstance;
+use Twilio\Rest\Verify\V2\ServiceContext;
+
+readonly class TwilioVerificationService
+{
+    private ServiceContext $verifyService;
+
+    public function __construct(private Client $client, private string $serviceId)
+    {
+        $this->verifyService = $this->client
+            ->verify
+            ->v2
+            ->services($this->serviceId);
+    }
+
+    public function sendVerificationCode(string $recipient, string $channel = 'SMS'): VerificationInstance
+    {
+        return $this->verifyService
+            ->verifications
+            ->create($recipient, $channel);
+    }
+
+    public function validateVerificationCode(
+        string $recipient,
+        string $code
+    ): VerificationCheckInstance {
+        return $this->verifyService
+            ->verificationChecks
+            ->create([
+                "to"   => "+" . $recipient,
+                "code" => $code,
+            ]);
+    }
+}
